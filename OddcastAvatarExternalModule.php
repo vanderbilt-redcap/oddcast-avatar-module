e096ad77e873966aab24a02776c854b37dd92f42
Mark McEver <mark@fastmail.net>
2017-05-15 13:42:40 -0500

    Added support for selecting characters

--- OddcastAvatarExternalModule.php
+++ OddcastAvatarExternalModule.php
@@ -29,7 +29,19 @@ class OddcastAvatarExternalModule extends AbstractExternalModule
 		</style>
 
 		<div id='oddcast-avatar' >
-			<script type="text/javascript" src="//vhss-d.oddcast.com/vhost_embed_functions_v2.php?acc=6267283&js=1"></script><script type="text/javascript">AC_VHost_Embed(6267283,300,400,'',1,1, 2556887, 2864965,1,0,'c01a5c706842a2c4fb1d468701b73ff6',0);</script>
+			<?php
+			$show = $this->getProjectSetting('character');
+			if($show == 'Amy'){
+				?>
+				<script type="text/javascript" src="//vhss-d.oddcast.com/vhost_embed_functions_v2.php?acc=6267283&js=1"></script><script type="text/javascript">AC_VHost_Embed(6267283,300,400,'',1,1, 2557406, 2865485,1,0,'19b9de5dfbb6523d8d0aa02e20f42102',0);</script>
+				<?php
+			}
+			else{
+				?>
+				<script type="text/javascript" src="//vhss-d.oddcast.com/vhost_embed_functions_v2.php?acc=6267283&js=1"></script><script type="text/javascript">AC_VHost_Embed(6267283,300,400,'',1,1, 2556887, 2864965,1,0,'c01a5c706842a2c4fb1d468701b73ff6',0);</script>
+				<?php
+			}
+			?>
 		</div>
 
 		<script>
