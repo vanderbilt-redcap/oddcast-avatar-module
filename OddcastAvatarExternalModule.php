d8432f1a9297644843a138dac84ef1a74279639d
oriapp <VICTR.Server.Admins@vanderbilt.edu>
2017-08-08 12:14:43 -0500

    Added a namespace specific to this module

--- OddcastAvatarExternalModule.php
+++ OddcastAvatarExternalModule.php
@@ -1,5 +1,8 @@
 <?php
-namespace ExternalModules;
+namespace Vanderbilt\OddcastAvatarExternalModule;
+
+use ExternalModules\AbstractExternalModule;
+use ExternalModules\ExternalModules;
 
 class OddcastAvatarExternalModule extends AbstractExternalModule
 {
