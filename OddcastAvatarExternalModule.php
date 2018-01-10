e2c4ee775d469dcc3095400e985b8afa63c840ef
Mark McEver <mark@fastmail.net>
2018-01-10 13:56:53 -0600

    Prevented surveys from being too skinny if they don't contain longer field names or other content

--- OddcastAvatarExternalModule.php
+++ OddcastAvatarExternalModule.php
@@ -55,6 +55,7 @@ class OddcastAvatarExternalModule extends AbstractExternalModule
 				height: 100vh;
 				overflow-y: scroll;
 				-webkit-overflow-scrolling: touch;
+				min-width: 724px; /* After adding the width of the avatar, this will fill the screen of a landscape ipad. */
 			}
 
 			#oddcast-avatar{
