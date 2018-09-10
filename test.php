e423d4a27e18a20d134a4bad713f23a6cd7a79ac
Mark McEver <mark@fastmail.net>
2018-09-10 13:49:16 -0500

    Removed an old unused file

--- test.php
+++
@@ -1,4 +0,0 @@
-<script type="text/javascript" src="//vhss-d.oddcast.com/vhost_embed_functions_v2.php?acc=6267283&js=1"></script><script type="text/javascript">AC_VHost_Embed(6267283,300,400,'',1,1, 2556887, 0,1,0,'764462e5aecc298854f79f4bbdf18db0',0);</script>
-
-<button onclick="javascript:loadScene(1);">Load Scene Index 1</button>
-<button onclick="javascript:loadScene(2);">Load Scene Index 2</button>
